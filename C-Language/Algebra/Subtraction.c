ef3fc69c753aded11bb025e0f02b3c596120c40d
khanbaba00 <72959540+khanbaba00@users.noreply.github.com>
2020-11-09 15:38:41 +0530

    Algebra

---
+++ C-Language/Algebra/Subtraction.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    /*Variable declaraton*/
+    int num1, num2, sub;
+
+    /*Appointing value*/
+    num1 = 50;
+    num2 = 30;
+
+    /*Subtracting values*/
+    sub = num1 - num2;
+
+    /*Printing the answer*/
+    printf("Output of %d - %d is : %d", num1, num2, sub);
+
+    return 0;
+}
